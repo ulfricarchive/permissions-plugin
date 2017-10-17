7c77218b0d20726bac96d7231b392b221520e0ed
Adam Harrison <ad@mharrison.us>
2017-10-17 13:35:22 +0300

    Update for changes in rethink-dragoon

--- src/main/java/com/ulfric/plugin/permissions/persistence/groups/GroupSystem.java
+++ src/main/java/com/ulfric/plugin/permissions/persistence/groups/GroupSystem.java
@@ -1,5 +1,6 @@
 package com.ulfric.plugin.permissions.persistence.groups;
 
+import com.ulfric.dragoon.extension.inject.Inject;
 import com.ulfric.dragoon.rethink.Database;
 import com.ulfric.dragoon.rethink.Store;
 import com.ulfric.plugin.entities.Entity;
@@ -7,6 +8,7 @@ import com.ulfric.plugin.entities.EntitySystem;
 
 public class GroupSystem extends EntitySystem {
 
+	@Inject
 	@Database(value = "permissions", table = "groups")
 	private Store<Entity> store;
 
--- src/main/java/com/ulfric/plugin/permissions/persistence/users/UserSystem.java
+++ src/main/java/com/ulfric/plugin/permissions/persistence/users/UserSystem.java
@@ -1,5 +1,6 @@
 package com.ulfric.plugin.permissions.persistence.users;
 
+import com.ulfric.dragoon.extension.inject.Inject;
 import com.ulfric.dragoon.rethink.Database;
 import com.ulfric.dragoon.rethink.Store;
 import com.ulfric.plugin.entities.Entity;
@@ -7,6 +8,7 @@ import com.ulfric.plugin.entities.EntitySystem;
 
 public class UserSystem extends EntitySystem {
 
+	@Inject
 	@Database(value = "permissions", table = "users")
 	private Store<Entity> store;
 
